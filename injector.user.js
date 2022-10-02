859e30977e219ef0c44826bfab70e627953aed17
Gleb Buzin <qufiwefefwoyn@gmail.com>
2022-10-02 14:56:09 +0300

    feat: soft image handling

--- injector.user.js
+++ injector.user.js
@@ -9,51 +9,95 @@
 // ==/UserScript==
 
 setTimeout(() => {
+    // Dict div
+    const newDiv = document.createElement("div");
+    document.getElementsByClassName("hgbeOc")[0].appendChild(newDiv);
+    newDiv.style.position = "fixed";
+    newDiv.style.right = "48px";
+    newDiv.style.backgroundColor = "white";
+    newDiv.style.padding = "10px";
+    newDiv.style.border = "1px solid #dadce0";
+    newDiv.style.borderRadius = "8px";
+    newDiv.style.zIndex = "999";
+    newDiv.style.boxShadow = "0 1px 4px 0 rgba(0,0,0,.37)";
 
-  const newDiv = document.createElement("div");
-  document.getElementsByClassName("hgbeOc")[0].appendChild(newDiv);
-  newDiv.style.position = "fixed";
-  newDiv.style.right = "48px";
-  newDiv.style.backgroundColor = "white";
-  newDiv.style.padding = "10px";
-  newDiv.style.border = "1px solid #dadce0";
-  newDiv.style.borderRadius = "8px";
-  newDiv.style.zIndex = "999";
-  newDiv.style.boxShadow = "0 1px 4px 0 rgba(0,0,0,.37)";
+    // Image div
+    const imgDiv = document.createElement("div");
+    document.getElementsByClassName("ccvoYb")[0].appendChild(imgDiv);
+    imgDiv.style.border = "1px solid #dadce0";
+    imgDiv.style.boxShadow = "0 1px 4px 0 rgba(0,0,0,.37)";
+    imgDiv.style.zIndex = "9998";
+    imgDiv.style.position = "fixed";
+    // imgDiv.style.margin = "auto";
+    // imgDiv.style.width = "auto";
+    // imgDiv.style.maxHeight = "200px";
+    imgDiv.style.right = "0px";
+    imgDiv.style.bottom = "0px";
 
-  var currentPage = location.href;
-  var currentText = '';
+    var currentPage = location.href;
+    var currentText = "";
 
-  setInterval(function () {
-    currentPage = location.href;
+    setInterval(function() {
+        currentPage = location.href;
 
-    var textarea = document.getElementsByTagName('textarea')[0];
-    text = textarea.value.trim()
-    if (text === currentText) {
-      return
-    } else {
-      currentText = text
-    }
-
-    let searchTerm = text
-    if (text.indexOf(' ') > -1) {
-      let split = text.split(' ');
-      searchTerm = split[split.length - 1].trim();
-    }
-    let urlToFetch = `http://localhost:8000/${searchTerm}`
-
-    fetch(urlToFetch)
-      .then((result) => {
-        if (!result.ok) {
-          alert('request failed');
+        var textarea = document.getElementsByTagName("textarea")[0];
+        text = textarea.value.trim();
+        if (text === "") {
+            imgDiv.innerHTML = ``;
+            return;
         }
-        return result.text();
-      })
-      .then((content) => {
-        const resp = JSON.parse(content);
-        const respDef = resp.def;
-        newDiv.innerHTML = respDef;
-      });
-  }, 500);
+        if (text === currentText) {
+            return;
+        } else {
+            currentText = text;
+            imgDiv.innerHTML = ``;
+        }
 
-}, 100)
+        let searchTerm = text;
+        if (text.indexOf(" ") > -1) {
+            let split = text.split(" ");
+            searchTerm = split[split.length - 1].trim();
+        }
+        if (searchTerm.length <= 2) {
+            return;
+        }
+        let urlToFetch = `http://localhost:8000/${searchTerm}`;
+
+        fetch(urlToFetch)
+            .then((result) => {
+                if (!result.ok) {
+                    alert("request failed");
+                }
+                return result.text();
+            })
+            .then((content) => {
+                const resp = JSON.parse(content);
+                const respDef = resp.def;
+                newDiv.innerHTML = respDef;
+                const hint = resp.hint;
+                if (hint != "") {
+                    // Image
+                    fetch(`https://imsea.herokuapp.com/api/1?q=${hint}`)
+                        .then((result) => {
+                            if (!result.ok) {
+                                alert("image request failed");
+                            }
+                            return result.text();
+                        })
+                        .then((content) => {
+                            const resp = JSON.parse(content);
+                            const imageUrl = resp.results[0];
+                            const imageUrl2 = resp.results[2];
+                            const imageUrl3 = resp.results[4];
+                            imgDiv.innerHTML = `
+<img id="term-image" style="max-height: 150px; width: auto;" src="${imageUrl}" alt="${hint} image">
+<img id="term-image2" style="max-height: 150px; width: auto;" src="${imageUrl2}" alt="${hint} image2">
+<img id="term-image3" style="max-height: 150px; width: auto;" src="${imageUrl3}" alt="${hint} image3">
+`;
+                        });
+                } else {
+                    imgDiv.innerHTML = ``;
+                }
+            });
+    }, 500);
+}, 100);
--- main.py
+++ main.py
@@ -8,6 +8,7 @@ parser = WiktionaryParser()
 
 NOT_FOUND = "not found"
 
+
 @app.get("/")
 def read_root():
     return {"def": "&nbsp"}
@@ -22,8 +23,8 @@ def bd(string):
 
 
 def style(short: str) -> str:
-    if hasattr(short, 'decode') and callable(getattr(short, 'decode')):
-        short = short.decode('utf-8')
+    if hasattr(short, "decode") and callable(getattr(short, "decode")):
+        short = short.decode("utf-8")
     decoded = short.replace(" ", " ")
     if decoded == NOT_FOUND:
         return decoded
@@ -43,34 +44,60 @@ def style(short: str) -> str:
     return new
 
 
+def remove_text_inside_brackets(text, brackets="()[]"):
+    count = [0] * (len(brackets) // 2)  # count open/close brackets
+    saved_chars = []
+    for character in text:
+        for i, b in enumerate(brackets):
+            if character == b:  # found bracket
+                kind, is_close = divmod(i, 2)
+                count[kind] += (-1) ** is_close  # `+1`: open, `-1`: close
+                if count[kind] < 0:  # unbalanced bracket
+                    count[kind] = 0  # keep it
+                else:  # found bracket to remove
+                    break
+        else:  # character is not a [balanced] bracket
+            if not any(count):  # outside brackets
+                saved_chars.append(character)
+    return "".join(saved_chars)
+
+
 def recursion(word: str) -> dict:
     cached = r.get(word)
-    if cached:
-        return {"def": cached, "source": "cache"}
+    cachedHint = r.get(word + "_hint")
+    if cached and cachedHint:
+        return {"def": cached, "hint": cachedHint, "source": "cache"}
 
     try:
         fetched = parser.fetch(word, "german")
         try:
             working = fetched[0]["definitions"][0]
             short = working["text"][0]
-
-            hint = working["text"][1]
-            if short == word and " of " in hint:
-                nextToSearch = hint.split(" ")[-1]
-                nextToSearch = nextToSearch.replace(":", "")
-                return recursion(nextToSearch)
+            hint = ""
+            if len(working["text"]) >= 2:
+                hint = working["text"][1]
+                if short == word and " of " in hint:
+                    nextToSearch = hint.split(" ")[-1]
+                    nextToSearch = nextToSearch.replace(":", "")
+                    return recursion(nextToSearch)
+                if working["partOfSpeech"] in ["preposition", "pronoun"]:
+                    hint = ""
+                else:
+                    hint = remove_text_inside_brackets(hint).strip()
 
             r.set(word, short)
-            return {"def": short, "source": "wikitionary"}
+            r.set(word + "_hint", hint)
+            return {"def": short, "hint": hint, "source": "wikitionary"}
         except:
             r.setex(word, 100, NOT_FOUND)
-            return {"def": NOT_FOUND}
+            r.setex(word + "_hint", 100, "")
+            return {"def": NOT_FOUND, "hint": ""}
     except:
         print("failed to fetch definition fron wikitionary")
-        return {"def": "wikitionary request failed"}
+        return {"def": "wikitionary request failed", "hint": ""}
 
 
 @app.get("/{word}")
 def read_item(word: str):
     answer = recursion(word)
-    return {"def": style(answer["def"])}
+    return {"def": style(answer["def"]), "hint": answer["hint"]}
